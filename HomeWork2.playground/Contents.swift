293616931ec22b76c99aa665c77f659c066ad074
Karlis B <karlis.butins@gmail.com>
2021-01-27 13:11:20 +0200

    Homework2

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -7,10 +7,10 @@ import UIKit
  1.3 Print the result with print ()
  */
 
-//var numberOne: Float = 6.56
-//var numberTwo: Float = 78.94
-//var numberThree: Double = Double(numberOne) + Double(numberTwo)
-//print(numberThree)
+var numberUno = 6.56
+var numberDue = 78.94
+var numberTres = Double(numberUno) + Double(numberDue)
+print(numberTres)
 
 
 /*
@@ -24,9 +24,9 @@ import UIKit
 
 var numberOne = 5
 var numberTwo = 4
-let constantResult = numberOne / numberTwo
-
-print("When dividing \(numberOne) by \(numberTwo) ")
+let divided = numberOne / numberTwo
+let remainder = numberOne % numberTwo
+print("When dividing \(numberOne) by \(numberTwo) the result is \(divided), remainder is \(remainder)")
 
 /*
  Ex3:
@@ -34,18 +34,69 @@ print("When dividing \(numberOne) by \(numberTwo) ")
  If you are buying 5 and more, with discount it will cost you 900 each! If you are buying 10 and more with discount it will cost you 850 each! Create if-else statements to check Conditions of buying in different amount!
  print("new: \(qty) MacBook Pro with the price of: \(price) EUR, will cost you: \(totalSum) Eur")
  */
+
+var price = 1000
+var qty = 11
+if qty >= 10 {
+    price = 850
+} else if qty >= 5 {
+    price = 900
+}
+var totalSum = qty * price
+print("new: \(qty) MacBook Pro with the price of: \(price) EUR, will cost you: \(totalSum) Eur")
+
 /*
+ 
  Ex4:
  Create String userInputAge and put value "33a" and convert to Int to make Fatal error: Unexpectedly found nil while unwrapping an Optional value!
  Fix this Fatal error inside the if-else statements to print whenever this age can be converted to Int or not!
  */
+
+let userInputAge = "33a"
+if let age = Int(userInputAge) {
+    print(age)
+} else {
+    print("can't")
+}
+
 /*
+ 
  Ex5:
  Calculate the number of years, months, days from you birthday to current date.
  print("Total years: \(totalYearsFromBirth) , total months: \(totalMonthFromBirth), total days: \(totalDaysFromBirth)")
  */
+
+let birthdayYear = 1987
+let nowYear = 2021
+let monthsInYear = 12
+let daysInYear = 365.25
+let totalYearsFromBirth = nowYear - birthdayYear
+let totalMonthsFromBirth = monthsInYear * totalYearsFromBirth
+let totalDaysFromBirth = Int(daysInYear) * totalMonthsFromBirth
+print("Total years: \(totalYearsFromBirth) , total months: \(totalMonthsFromBirth), total days: \(totalDaysFromBirth)")
+
+
 /*
  Ex6:
  Use Exercise 5 monthOfBirth to calculate in which quarter of the year you were born.
  Use switch case to print("I was born in the ... quarter")
  */
+
+let birthdayMonth = 10
+var countQuarter = ""
+switch birthdayMonth {
+case 1...3:
+    countQuarter = "first quarter"
+case 3...6:
+    countQuarter = "second quarter"
+case 6...9:
+    countQuarter = "third quarter"
+case 9...12:
+    countQuarter = "fourth quarter"
+default:
+    countQuarter = "Un-reborn again"
+}
+print("I was born in the \(countQuarter) of the year \(birthdayYear)")
+
+
+
